4d45189474888d8140d815c41ee3984e7cc9bc03
above-max <ms-161140@hs-weingarten.de>
2019-06-26 10:49:43 +0200

    minor updates in END version

--- End/Count.gd
+++ End/Count.gd
@@ -4,14 +4,9 @@ extends Label
 var counter = 0
 # var b = "text"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.text = str(counter)
-	pass
 
 func update_count_label():
 	counter = counter + 1
